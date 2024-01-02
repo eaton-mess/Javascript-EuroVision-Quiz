9bda55be259b6299f62953a5a2ba671f4ac588f7
eatontechnology <eaton.technology@gmail.com>
2024-01-02 13:05:02 +0000

    added questions, options and answers

--- assets/script/script.js
+++ assets/script/script.js
@@ -29,4 +29,85 @@ var highScorePage = document.querySelector("#highscores");
 var backBtn = document.querySelector("#go-back");
 var clearBtn = document.querySelector("#clear-scores");
 
+//defining questions
+var questionSource = [
+    {
+        question:
+            "Question 1 : Which country hosted the first ever Eurovision Song Contest in 1956?",
+        choices: [
+            "a. UK",
+            "b. Switzerland",
+            "c. Germany",
+            "d. France"],
+        answer: "b",
+    },
+    {
+        question: "Question 2 : How many times has Sweden won Eurovision?:",
+        choices: [
+            "a. one",
+            "b. three",
+            "c. five",
+            "d. seven"],
+        answer: "d",
+    },
+    {
+        question: "Question 3 : In what year did ABBA win Eurovision with the song 'Waterloo'?",
+        choices: [
+            "a. 1971",
+            "b. 1974",
+            "c. 1975",
+            "d. 1972",
+        ],
+        answer: "b",
+    },
+    {
+        question: "Question 4 : From what year were entrants NO LONGER required to sing in their country's official language?",
+        choices: [
+            "a. 1980",
+            "b. 1992",
+            "c. 1999",
+            "d. 2001",
+        ],
+        answer: "c",
+    },
+    {
+        question:
+            "Question 5 : As of 2023, which country has received the most public votes in any given year?",
+        choices: [
+            "a. Finland",
+            "b. Sweden",
+            "c. Portugal",
+            "d. Ukraine"],
+        answer: "d",
+    },
+    {
+        question: "Question 6 : Since 2009, taking on after Terry Wogan, who has famously commentated Eurovision for the BBC?",
+        choices: [
+            "a. Graham Norton",
+            "b. Jeremy Clarkson",
+            "c. Alan Carr",
+            "d. Boris Johnson"],
+        answer: "a",
+    },
+    {
+        question: "Question 7 : Who holds the record for the most 'nil-pois'?",
+        choices: [
+            "a. UK",
+            "b. Germany",
+            "c. Norway",
+            "d. France"],
+        answer: "c",
+    },
+    {
+        question:
+            "Question 8 : Eurovision has only ever been cancelled once. In what year?",
+        choices: [
+            "a. 1964",
+            "b. 2001",
+            "c. 2020",
+            "d. 1991"],
+        answer: "c",
+    },
+];
+
 
